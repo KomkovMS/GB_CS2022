faaa66bcb10cbc7e8f0174ff958050bbd8d33ff5
KomkovMS <maxkoma05@gmail.com>
2022-10-09 17:43:23 +0700

    Решение дополнительной задачи 7 из списка на семинаре 2 (доп)

---
+++ Seminar2/task_dop7/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+7. Показать числа от -N до N
+*/
+
+string getNumN(int n)
+{
+    int m = -n;
+    string text = "";
+
+    while (m <= n)
+    {
+        text = text + $" {m} ";
+        m++;
+    }
+    return text;
+}
+
+string result = getNumN(5);
+Console.WriteLine(result);
