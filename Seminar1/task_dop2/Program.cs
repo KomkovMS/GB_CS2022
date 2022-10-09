b637ec0cf4bebeb18dd8925420108297e04e6197
KomkovMS <maxkoma05@gmail.com>
2022-10-09 14:33:10 +0700

    Решил дополнительно задачу 2 из списка

---
+++ Seminar1/task_dop2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+2. Даны два числа. Показать большее и меньшее число
+*/
+
+void getNaxValue(int val1, int val2)
+{
+    int maxVal = val1;
+    if (val2 > maxVal)
+    {
+        maxVal = val2;
+    }
+    Console.WriteLine("Max value: " + maxVal);
+}
+
+void getRandomNum(int start, int end)
+{
+    int val1 = new Random().Next(start, end);
+    Console.WriteLine("Первое число: " + val1);
+    int val2 = new Random().Next(start, end);
+    Console.WriteLine("Второе число: " + val2);
+    getNaxValue(val1, val2);
+}
+
+getRandomNum(-100, 100);
+
