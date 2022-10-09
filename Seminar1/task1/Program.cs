e12755097c5eb9de5ebaf5fb61256911126c9128
KomkovMS <maxkoma05@gmail.com>
2022-10-09 13:26:59 +0700

    Добавлено решение первой задачи

--- Seminar1/task1/Program.cs
+++ Seminar1/task1/Program.cs
@@ -1,7 +1,10 @@
-﻿// 1. Напишите программу, которая на вход принимает число и выдает его квадрат (число, умноженное на само себя)
-// Например: 4 -> 16
-//          -3 -> 9
-//          -7 -> 49
+﻿/*
+1. Напишите программу, которая на вход принимает число и выдает его квадрат 
+(число, умноженное на само себя)
+Например: 4 -> 16
+          -3 -> 9
+          -7 -> 49
+*/
 
 // мое решение
 int number = new Random().Next(-10, 10);
@@ -21,3 +24,12 @@ string input = Console.ReadLine();
 int a = Convert.ToInt32(input);
 int result = a * a;
 Console.WriteLine("Квадрат числа " + a + " = " + result);
+
+// на семинаре преподаватель (2)
+Console.WriteLine("start");
+
+int value = 4;
+int result2 = value * value;
+Console.WriteLine(result2);
+
+Console.WriteLine("end");
