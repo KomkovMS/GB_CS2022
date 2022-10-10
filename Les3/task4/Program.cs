ad4523d25a7d1f6debfe1b13614dddd2c1861a9e
KomkovMS <maxkoma05@gmail.com>
2022-10-10 21:11:31 +0700

    Лекция 3 - решение задачи с сортировкой массива от минимального

---
+++ Les3/task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Работа с массивами
+
+// 4. Упорядочивание данных в массиве
+
+// Алгоритм сортировки (выбор)
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// Метод, который будет упорядочивать наш массив
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+            {
+        }
+
+        int tempory = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = tempory;
+
+        }
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
