4fcfaa499d965eb4327b9eaa93c275a775e1cd7b
KomkovMS <maxkoma05@gmail.com>
2022-10-10 23:35:03 +0700

    Семинар 3 - решение задачи 1 (сам - апгрейд)

---
+++ Seminar3/task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+2. Задача 18: 
+Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+возможных координат точек в этой четверти (x и y).
+*/
+
+
+// мое решение
+
+void outputNum(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    getPoint(num);
+}
+
+
+void getPoint(int num)
+{
+    if (num == 1) {
+    Console.WriteLine($"Для четверти: {num} диапазон возможных координат точек: X > 0, Y > 0");
+    }
+    else if (num == 2) {
+        Console.WriteLine($"Для четверти: {num} диапазон возможных координат точек: X < 0, Y > 0");
+    }
+    else if (num == 3) {
+        Console.WriteLine($"Для четверти: {num} диапазон возможных координат точек: X < 0, Y < 0");
+    } else {
+        Console.WriteLine($"Для четверти: {num} диапазон возможных координат точек: X > 0, Y < 0");
+    }
+}
+
+
+string message = "Введите номер четверти плоскости: ";
+outputNum(message);
+
+
+// решение на семинаре
+
