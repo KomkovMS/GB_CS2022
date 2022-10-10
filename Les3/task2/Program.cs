e84113d19a0bc230c3dec7f8d8860e0e7e314942
KomkovMS <maxkoma05@gmail.com>
2022-10-10 20:17:01 +0700

    Лекция 3 (функции - продолжение) + цикл for

---
+++ Les3/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Вид 4 - что то принимают и что-то возвращают
+
+// Цикл for
+
+string Method4(int count, string text)
+{
+    string result = String.Empty; 
+    for (int i = 0; i < count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
+
+string res = Method4(10, "w");
+Console.WriteLine(res);
+
+
+// Цикл в цикле
+
+// Выведем таблицу умножение на экран
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine(" ");
+}
