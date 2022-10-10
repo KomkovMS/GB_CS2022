751acf3dfc5d8ba8a41f76e8272f41c356f05364
KomkovMS <maxkoma05@gmail.com>
2022-10-10 22:23:31 +0700

    Домашнее задание 2 задача 3 - пример решения другого ученика на семинаре 3

--- DZ2/h_task3/Program.cs
+++ DZ2/h_task3/Program.cs
@@ -8,6 +8,8 @@
 
 */
 
+// мое решение
+
 void getDay (int day, string[] arrDays)
 {
     if (arrDays[day] == "Sun" || arrDays[day] == "Sat")
@@ -22,8 +24,6 @@ void getDay (int day, string[] arrDays)
 
 void enterNumDay ()
 {
-    
-
     while (true)
     {
         Console.WriteLine("Enter the day of the week: ");
@@ -49,3 +49,25 @@ void enterNumDay ()
 
 enterNumDay ();
 
+// пример решения другого ученика на семинаре 3
+
+Console.WriteLine("Задача №15");
+Console.WriteLine("----------------------------------------");
+
+string[] day = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+
+Console.Write("Введите целое число (1-7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    Console.Write("Это " + day[number - 1]);
+    if (number==6 || number==7)
+    {
+        Console.Write(" - выходной день.");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня не существует!");
+}
