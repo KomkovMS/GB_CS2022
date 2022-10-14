e6ceadf2589b0110cacdd33a2897a2d13f023423
KomkovMS <maxkoma05@gmail.com>
2022-10-14 21:43:21 +0700

    Семинар 4 - задача 2 (26) - Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе (мое решение)

---
+++ Seminar4/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+2. Задача 26: Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе.
+
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+// мое решение
+
+int getCountNum(string msg, int num)
+{
+    int res = 0;
+    for (int idx = 1; idx <= num; idx++)
+    {
+        
+        num = num / 10;
+        res++;
+    }
+
+     return res;
+}
+
+
+string msg = "Введите число: ";
+Console.WriteLine(msg);
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(" -> ");
+Console.Write(getCountNum(msg, num));
