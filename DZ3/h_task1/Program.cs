123f2aebc826197f396f407fc6a307497a37cf8b
KomkovMS <maxkoma05@gmail.com>
2022-10-14 20:13:27 +0700

    Семинар 4 -проверка числа на  палиндром

--- DZ3/h_task1/Program.cs
+++ DZ3/h_task1/Program.cs
@@ -53,3 +53,52 @@ void getNum(string text)
 }
 
 getNum("Введите пятизначное число для проверки на палиндром: ");
+
+
+// Решение преподавателя на Семинаре 4 (вар.1)
+
+bool isPalindrome(int x) {                        // 11211
+    int div = 1;                                  // 1
+    while (x / div >= 10) {                       // 11211 / 100000 >= 10
+        div *= 10;                                // 100000 * 10 = 1000000
+    }
+    Console.WriteLine(div);
+    while (x != 0) {                              // 11211                  
+        int l = x / div;                          // 11211 / 100000 = 0
+        int r = x % 10;                           // 11211 % 10 = 1
+        if (l != r) {                              // 0 != 1
+            Console.WriteLine("Not Lalindrome");   // Not Lalindrome
+            return false;
+        }
+
+        x = (x % div) / 10;                 
+        div /= 100;
+    }
+    Console.WriteLine("Palindrom");
+    return true;
+}
+
+// Решение преподавателя на Семинаре 4 (вар.2)
+
+bool isPalindromeLight(int[] arr) {
+    bool result = false;
+    const N = arr.Lenght;
+    for (int i = 0; i < N / 2; i++) {
+        if (arr[i] != arr[N - i - 1]) {
+            Console.WriteLine("Not Polindrom");
+            return result;
+        }
+    }
+    result = true;
+    Console.WriteLine("Polindrom");
+
+    return result;
+}
+
+isPalindrome(1231);
+isPalindrome(11211);
+isPalindrome(1221);
+
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(''), int.Parse);
+isPalindromeLight(arr);
---
+++ Seminar4/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿bool isPalindromeLight(int[] arr) {
+    bool result = false;
+    const N = arr.Lenght;
+    for (int i = 0; i < N / 2; i++) {
+        if (arr[i] != arr[N - i - 1]) {
+            Console.WriteLine("Not Polindrom");
+            return result;
+        }
+    }
+    result = true;
+    Console.WriteLine("Polindrom");
+
+    return result;
+}
+
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+isPalindromeLight(arr);
