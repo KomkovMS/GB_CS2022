a57e23376929a755cbcf4b11cdfc4452af8e795c
KomkovMS <maxkoma05@gmail.com>
2022-10-14 20:58:58 +0700

    Семинар 4 - задача 1 (24) Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт

--- Seminar4/task1/Program.cs
+++ Seminar4/task1/Program.cs
@@ -1,18 +1,29 @@
-﻿bool isPalindromeLight(int[] arr) {
-    bool result = false;
-    const N = arr.Lenght;
-    for (int i = 0; i < N / 2; i++) {
-        if (arr[i] != arr[N - i - 1]) {
-            Console.WriteLine("Not Polindrom");
-            return result;
-        }
-    }
-    result = true;
-    Console.WriteLine("Polindrom");
+﻿/*
+1. Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт 
+сумму чисел от 1 до А.
 
-    return result;
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+// мое решение
+
+int getSum(string msg, int num)
+{
+    int sum = 0;
+    for (int idx = 1; idx <= num; idx++)
+    {
+        sum += idx; 
+    }
+
+    return sum;
 }
 
 
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-isPalindromeLight(arr);
+string msg = "Введите число: ";
+Console.WriteLine(msg);
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(" -> ");
+Console.Write(getSum(msg, num));
+
