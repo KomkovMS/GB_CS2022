50f2847ac7550db239e1593c47775af30adab6ca
KomkovMS <maxkoma05@gmail.com>
2022-10-15 12:09:51 +0700

    Домашнее задание 1 - задача 1 (25) - решение

---
+++ DZ4/h_task1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+1) Задача 25: 
+Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+*/
+
+
+int NumPow(int a, int b)
+{
+    int result = Convert.ToInt32(Math.Pow(a, b));
+    
+    return result;
+}
+
+
+void EnterNum(string msg)
+{
+    Console.Write(msg);
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write(msg);
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    PrintNum(num1, num2);
+}
+
+void PrintNum(int num1, int num2)
+{
+    Console.Write("-> ");
+    Console.Write(NumPow(num1, num2));
+}
+
+EnterNum("Enter number: ");
+
