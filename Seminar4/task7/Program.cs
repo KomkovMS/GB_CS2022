fa69e052b74c72b6eb20910ae4a5e8baa4acd764
KomkovMS <maxkoma05@gmail.com>
2022-10-15 20:29:27 +0700

    Семинар 4 - Показать сколько каких элементов присутствует в массиве (НЕ РЕШЕНО)

--- Seminar4/task7/Program.cs
+++ Seminar4/task7/Program.cs
@@ -8,7 +8,7 @@
 
 */
 
-// int n = Random.Shared.Next(1, 20); // 10<= n <= 1000
+// int n = Random.Shared.Next(1, 100); // 10<= n <= 1000
 // // int[]arr = new int[n]; // 0 .. 100
 
 // int[]arr = Enumerable.Range(0, n)
@@ -18,11 +18,34 @@
 // Console.WriteLine(String.Join(" ", arr));
 // Console.WriteLine(arr.Length);
 
-// int count = 0;
-// for (int i = 0; i < arr.Length; i++)
+// int count1 = 0;
+// int count2 = 0;
+// int count3 = 0;
+// for (int i = 0; i < (arr.Length - 1); i++)
 // {
-    
+//     for (int j = i + 1; j < arr.Length; j++)
+//     {
+//         if (arr[i] == count3)
+//         {
+//             continue;
+//         }
+//     }
+//     for (int j = i + 1; j < arr.Length; j++)
+//     {
+//         if (arr[i] == arr[j])
+//         {
+//             count1++;
+//             count2++;
+//             count3 = arr[i];
+//         }
+//     }
+//     if (count1 > 0)
+//     {
+
+//          Console.WriteLine($"{arr[i]} повторяется в массиве {count1} раз");
+//     }
 // }
+
 
 
 // НЕОБХОДИМО ВЕРНУТЬ К РЕШЕНИЮ ЗАДАЧИ
