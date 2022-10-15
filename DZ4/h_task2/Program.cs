fefa6facc2eee77df70913ce53abc9d8a9214919
KomkovMS <maxkoma05@gmail.com>
2022-10-15 12:43:45 +0700

    Домашнее задание 4 - задача 2 (27) - решение

---
+++ DZ4/h_task2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+2. Задача 27: 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+
+*/
+
+
+int NumSun(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum += a % 10;
+        a = a / 10;
+    }
+
+    return sum;
+}
+
+void EnterNum(string msg)
+{
+    Console.Write(msg);
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    PrintNum(num);
+}
+
+void PrintNum(int num)
+{
+    Console.Write("-> ");
+    Console.Write(NumSun(num));
+}
+
+
+EnterNum("Enter the number: ");
+
+
---
+++ DZ4/h_task3/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = 452;
+
+for (int i = 0; i < a; i++)
+{
+    a += a % 10;
+    Console.WriteLine("Первое число" + a);
+}
