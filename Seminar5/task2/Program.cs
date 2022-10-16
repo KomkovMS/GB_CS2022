e345c735ff8aac308d12cc01ddc03e2d307b3f5a
KomkovMS <maxkoma05@gmail.com>
2022-10-16 16:49:13 +0700

    Семинар 5 - задача 2 (32) - мое решение (амена элементов массив)а

---
+++ Seminar5/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+2. Задача 32: 
+Напишите программу замена элементов массива: положительные элементы замените 
+на соответствующие отрицательные, и наоборот.
+
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+// мое решение
+
+// метод, который делает замену элементов массива
+
+int[] GetReplacingElemOfArray(int[] col)
+{
+    int[] newCol = col;
+    for (int i = 0; i < col.Length; i++)
+    {
+        newCol[i] *= -1;
+    }
+    return newCol;
+}
+
+
+int[] array = {-4, -8, 8, 2};
+var strArray = string.Join(", ", array);
+int[] newArr = GetReplacingElemOfArray(array);
+var strNewArr = string.Join(", ", newArr);
+Console.Write($"[{strArray}] -> [{strNewArr}]");
+
+
