d30c041f16d10b0fd31e27ceb9a74a33d586fb12
KomkovMS <maxkoma05@gmail.com>
2022-10-16 18:51:08 +0700

    Семинар 5 - задача 3 (33) - мое решение. присутствует ли заданное число в массиве

---
+++ Seminar5/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+3. Задача 33: Задайте массив. Напишите программу, которая определяет, 
+присутствует ли заданное число в массиве.
+
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+
+*/
+
+// мое решение
+
+string СheckNumber(int[] col, int NumCheck)
+{
+    string result = string.Empty;
+    for (int i = 0; i < col.Length; i++)
+    {
+        if (col[i] == Math.Abs(NumCheck)) 
+        {
+            result = "да";
+        } else {
+            result = "нет";
+        }
+    }
+
+    return result;
+}
+
+
+int[] array = {6, 7, 19, 345, 3};
+var strArray = string.Join(", ", array);
+Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numCheck = СheckNumber(array, number);
+Console.Write($"[{strArray}] -> {numCheck}");
