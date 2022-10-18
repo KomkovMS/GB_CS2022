95d800978b86bb3097fe0193a303c1088ff636a2
KomkovMS <maxkoma05@gmail.com>
2022-10-19 00:08:12 +0700

    Семинар 5 (условие и решение  доп задачи с другого семинара) - Заполнить массив случайными числами (другой вариант)

---
+++ Seminar5/dop_task9/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Заполнить массив случайными числами (другой вариант)
+*/
+
+int GetItem(int el)
+{
+    return Random.Shared.Next(0, 30);
+}
+
+int[] arr = Enumerable.Range(1, 100)
+            .Select(GetItem)
+            .ToArray();
+
+
+Console.WriteLine(String.Join(", ", arr));
