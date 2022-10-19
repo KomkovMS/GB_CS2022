8e5001bb6ab260056dc8f42cb5b302460e9ac00c
KomkovMS <maxkoma05@gmail.com>
2022-10-20 00:08:47 +0700

    Лекция 6 Парсинг строки. Увеличить каждую координату на N

--- Les6/task1/Program.cs
+++ Les6/task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Парсинг строки. Увеличить каждую координату на N
+*/
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+              .Replace("(", "")       
+              .Replace(")", "")
+              ;
+
+var data = text.Split(" ")  // возьми текст и разбей его
+               .Select(item => item.Split(',')) 
+               .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+               .Where(e => e.x % 2 == 0)    // получим набор точек у которых первая координата четная
+               .Select(point => (point.x * 10, point.y))
+               .ToArray();  // превращение в явный массив
+
+for (var i = 0; i < data.Length; i++)
+{
+   Console.WriteLine(data[i]);
+}
+
