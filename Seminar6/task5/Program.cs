411a002081f87c35eed508f7bfe5b10b22b7ab12
KomkovMS <maxkoma05@gmail.com>
2022-10-22 17:06:29 +0700

    Семинар 6 - задача 5 (45)Напишите программу, которая будет создавать копию заданного массива

---
+++ Seminar6/task5/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+5. Задача 45: 
+Напишите программу, которая будет создавать копию заданного массива 
+с помощью поэлементного копирования
+
+*/
+
+// мое решение
+
+int[] arr = new int[7]{23, 2, 54, 47, 4, 8, 12};
+
+int[] arrNew = new int[7]; // Array.Empty<int>();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arrNew[i] = arr[i];
+}
+
+var str = string.Join(", ", arrNew);
+
+Console.Write(str);
