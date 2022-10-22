56956a90509e2f28ccd053d6d46f7c83d90c0f23
KomkovMS <maxkoma05@gmail.com>
2022-10-22 21:42:58 +0700

    Домашнее задание 6 - задача 1 (41) - Посчитайте, сколько чисел больше 0 ввёл пользователь

---
+++ DZ6/h_task1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+1. Задача 41: 
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+*/
+
+int[] GetArr(int n)
+{
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return arr;
+}
+
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+Console.WriteLine("Какое количество цифр вы хотите ввести? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = GetArr(n);
+
+int count = GetCount(array);
+
+var str = string.Join(", ", array);
+Console.WriteLine($"Вы ввели числа: {str} -> {count}");
+
+
+    
+
+
+
