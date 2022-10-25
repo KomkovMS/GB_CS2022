bca8cdd7e9f104908fa5399bae73aab5cb74807c
KomkovMS <maxkoma05@gmail.com>
2022-10-25 21:41:27 +0700

    Домашнее задание 7 - задача 3 (58) - Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц

--- DZ7/h_task3/Program.cs
+++ DZ7/h_task3/Program.cs
@@ -1,7 +1,8 @@
 ﻿/*
 3. Задача 58: 
 
-Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Задайте две матрицы. Напишите программу, которая будет находить произведение 
+двух матриц.
 
 Например, даны 2 матрицы:
 2 4 | 3 4
@@ -31,96 +32,52 @@ int[,] FillArray(int[,] matr,
     return matr;
 }
 
-// // метод, который сравнивает входящую позицию элемента в двумерном массиве,
-// // и возвращает результат
-// string GetElementPosition(int[,] matr, 
-//                        int m, 
-//                        int n, 
-//                        int number,
-//                        string txt)
-// {
-//     string result = string.Empty;
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             if (matr[i, j] == number) result = Convert.ToString(matr[i, j]); 
-//         } 
-//     }
-
-//     return result;
-// }
-
-// // метод, который печатает значение найденного элемента в массиве или 
-// // же указание, что такого элемента нет
-// void PrintResult(string result, string txt, int number)
-// {
-//     if (result == string.Empty) Console.WriteLine($"{number} -> {txt}");
-//     else Console.WriteLine($"{number}");
-// }
-
-// метод, который печатает начальные массивы
-void PrintArray(int[,] matr1, int[,] matr2, int m, int n)
+// метод, который печатает массивы - множители
+void PrintArray(int[,] matr, int m, int n)
 {
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            Console.Write($"{matr1[i, j]} ");
+            Console.Write($"{matr[i, j]} ");
         }
         Console.WriteLine();
     }
-
-    Console.WriteLine("----------");
-
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            Console.Write($"{matr2[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-
-
-
-
 }
 
-int[,] GerResult(int[,] matr1, int[,] matr2)
+// метод, который возвращает произведение матриц
+int[,] GerResult(int[,] matr1, int[,] matr2, int m, int n)
 {
-    for (int i = 0; i < matr1.GetLength(0); i++)
+    int[,] matrRes = new int[m, n];
+    
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < matr2.GetLength(1); j++)
+        for (int j = 0; j < n; j++)
         {
-            for (int y = 0; y < matr1.GetLength(1); y++)
+            for (int y = 0; y < m; y++)
             {
                 matrRes[i, j] += matr1[i, y] * matr2[y, j];
             }
         }
     }
+
     return matrRes;
+}
 
-    // int[,] res = new int[m, n];
+// метод, который печатает результат работы программы в консоль
+void PrintResult(int[,] result, int m, int n)
+{
+    Console.WriteLine("Результирующая матрица будет: ");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            
+            Console.Write($"{result[i, j]} ");
+        }
 
-    
-    // res[0,0] = matr1[0,0] * matr2[0,0] + matr1[0,1] * matr2[1,0];
-    // res[0,1] = matr1[0,0] * matr2[0,1] + matr1[0,1] * matr2[1,1];
-    // res[1,0] = matr1[1,0] * matr2[0,0] + matr1[1,1] * matr2[1,0];
-    // res[1,1] = matr1[1,0] * matr2[0,1] + matr1[1,1] * matr2[1,1];
-
-
-    // Console.WriteLine($"{res[0,0]} {res[0,1]}");
-    // Console.WriteLine($"{res[1,0]} {res[1,1]}");
-
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for (int j = 0; j < 3; j++)
-    //     {
-    //         Console.WriteLine(res[i,j]);
-    //     }
-    // }
-    
+        Console.WriteLine();
+    }
 }
 
 
@@ -141,20 +98,34 @@ int[,] matrixTwo = new int[m, n];
 int[,] ArrayOne = FillArray(matrixOne, m, n, min, max, rnd);
 int[,] ArrayTwo = FillArray(matrixTwo, m, n, min, max, rnd);
 
+// вызываем метод, который возвращает результат перемножения матриц
+int[,] result = GerResult(ArrayOne, ArrayTwo, m, n);
+
 
 // ---------- вывод результата в консоль -----------
+Console.WriteLine();
 
 // распечатаем получившиеся двумерные массивы
-PrintArray(ArrayOne, ArrayTwo, m, n);
+PrintArray(ArrayOne, m, n);
+Console.WriteLine("---");
+PrintArray(ArrayTwo, m, n);
 
-Console.WriteLine("================================");
+Console.WriteLine("=============================");
 
-int[,] result = GerResult(ArrayOne, ArrayTwo);
+// выводим массив, являющийся результатом перемножения матриц
+PrintResult(result, m, n);
+Console.WriteLine();
 
 
 
-// // вызываем метод, который вернет значение элемента
-// string elementPosition = GetElementPosition(matrix, m, n, number, txt);
 
-// // печатаем результат в виде элемента, если он есть или указание, что его нет)
-// PrintResult(elementPosition, txt, number);
+
+
+
+// res[0,0] = matr1[0,0] * matr2[0,0] + matr1[0,1] * matr2[1,0];
+// res[0,1] = matr1[0,0] * matr2[0,1] + matr1[0,1] * matr2[1,1];
+// res[1,0] = matr1[1,0] * matr2[0,0] + matr1[1,1] * matr2[1,0];
+// res[1,1] = matr1[1,0] * matr2[0,1] + matr1[1,1] * matr2[1,1];
+
+// Console.WriteLine($"{res[0,0]} {res[0,1]}");
+// Console.WriteLine($"{res[1,0]} {res[1,1]}");
---
+++ Seminar7/task_promo/Program.cs
@@ -0,0 +1,82 @@
+﻿// Task 58. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.Clear();
+Console.WriteLine("This program implements matrix multiplication: A x B = C.");
+Console.WriteLine();
+int numberRowMatrixA = EnterUserData("Enter number of rows for Matrix A: ");
+int numberColMatrixA = EnterUserData("Enter number of columns for Matrix A: ");
+int numberRowMatrixB = EnterUserData("Enter number of rows for Matrix B: ");
+int numberColMatrixB = EnterUserData("Enter number of columns for Matrix B: ");
+int lowerLim = EnterUserData("Enter lower limit for random range:");
+int upperLim = EnterUserData("Enter upper limit for random range:");
+int precision = EnterUserData("Enter precision for numbers: ");
+double[,] matrixA = new double[numberRowMatrixA, numberColMatrixA];
+double[,] matrixB = new double[numberRowMatrixB, numberColMatrixB];
+double[,] matrixResult = new double[numberRowMatrixA, numberColMatrixB];
+
+if (numberColMatrixA != numberRowMatrixB)
+{
+    Console.WriteLine("The number of columns of the Matrix A must be equal to the number of rows of the Matrix B! You cannot multiply these matrices!");
+    return;
+}
+
+Console.WriteLine();
+FillArray(matrixA, numberRowMatrixA, numberColMatrixA, lowerLim, upperLim, precision);
+Console.WriteLine("Matrix A:");
+PrintArray(matrixA);
+Console.WriteLine();
+
+FillArray(matrixB, numberRowMatrixB, numberColMatrixB, lowerLim, upperLim, precision);
+Console.WriteLine("Matrix B:");
+PrintArray(matrixB);
+Console.WriteLine();
+
+matrixResult = MatrixMultiply(matrixA, matrixB);
+Console.WriteLine("Multiplication A x B =");
+Console.WriteLine();
+PrintArray(matrixResult);
+
+double[,] MatrixMultiply(double[,] matrixA, double[,] matrixB)
+{
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            for (int n = 0; n < matrixA.GetLength(1); n++)
+            {
+                matrixResult[i, j] += matrixA[i, n] * matrixB[n, j];
+            }
+        }
+    }
+    return matrixResult;
+}
+
+double[,] FillArray(double[,] array, int numberRow, int numberCol, int lowerLim, int upperLim, int precision)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double randomNum = new Random().NextDouble() * (upperLim - lowerLim);
+            array[i, j] = Math.Round(randomNum, precision);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int EnterUserData(string nameUserData)
+{
+    Console.Write($"{nameUserData}");
+    return Convert.ToInt32(Console.ReadLine());
+}
