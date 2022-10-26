d00d908ba78fecbf017de8ec20db5e7f70cb345d
KomkovMS <maxkoma05@gmail.com>
2022-10-26 20:52:53 +0700

    Семинар 6 Определение производительности при переборе двумерного массива

---
+++ Seminar6/dop_task7/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Определение производительности при переборе двумерного массива
+*/
+Console.Clear();
+
+int n = 8000;
+int[,] m = new int[n, n];
+int s = 0;
+
+// time start
+DateTime ts = DateTime.Now;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        s += m[i, j];
+    }
+}
+DateTime te = DateTime.Now;
+System.Console.WriteLine((te-ts).TotalMilliseconds);
