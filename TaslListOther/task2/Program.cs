e1e4c28b5b2948bbd03f809c908a54cebb2878df
KomkovMS <maxkoma05@gmail.com>
2022-11-06 11:27:44 +0700

    Самостоятельная работа - задача 2 (18) - Проверить истинность утверждения !(X ||  Y) =  !X && ! Y

---
+++ TaslListOther/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+*/
+
+// Отрица́ние ¬ 
+// Дизъю́нкция ⋁ ИЛИ.
+// Конъю́нкция ⋀ «И»
+ 
+bool getLeftPart(bool x, bool y)
+{
+    bool leftPart = !(x || y);
+    return leftPart;
+}
+
+bool getRightPart(bool x, bool y)
+{
+    bool rightPart = (!x) && (!y);
+    return rightPart;
+}
+
+bool getBoolResult(bool leftPart, bool rightPart)
+{
+    return leftPart == rightPart;
+}
+
+
+bool x = true;
+bool y = true;
+bool leftPart = getLeftPart(x, y);
+bool rightPart = getRightPart(x, y);
+bool boolResult = getBoolResult(leftPart, rightPart);
+
+Console.WriteLine($"¬(X ⋁ Y) = {leftPart}");
+Console.WriteLine($"¬X ⋀ ¬Y = {rightPart}");
+Console.WriteLine($"¬(X ⋁ Y) = ¬X ⋀ ¬Y -> {boolResult}");
