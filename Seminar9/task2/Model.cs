f33f38242bd5beb942d804c500417316449bbfc9
KomkovMS <maxkoma05@gmail.com>
2022-11-15 23:49:26 +0700

    Решение задач на семинаре 9

---
+++ Seminar9/task2/Model.cs
@@ -0,0 +1,7 @@
+class Worker
+{
+    public int age;
+    public int salary;
+    public string fullName;
+}
+
---
+++ Seminar9/task6/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 63: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от 1 до N.
+
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+
+*/
+
+// мое решение циклом без рекурсии
+int n = 6;
+
+Console.Write($"{n} -> ");
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{i}, ");
+}
+
+
+// решение преподавателя на семинаре
+Console.WriteLine();
+
+Console.Write("Введите N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+string PrintNumbers(int start, int end)
+{
+    // Базовый случай
+    if (start == end) return start.ToString();
+    // Рекурсивный случай 
+    return (start + ", " + PrintNumbers(start + 1, end));
+}
+
+Console.Write($"{N} -> ");
+Console.Write($"{PrintNumbers(1, N)}");
