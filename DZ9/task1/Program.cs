062dde1db5d0e01a71cdbfe1ad9e0eab4c23f84e
KomkovMS <maxkoma05@gmail.com>
2022-11-17 20:23:43 +0700

    Домашнее задание 9 - решение

--- DZ9/task1/Program.cs
+++ DZ9/task1/Program.cs
@@ -1,7 +1,19 @@
 ﻿/*
-Задача 64: Задайте значения M и N. Напишите программу, которая выведет все 
-натуральные числа в промежутке от M до N.
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
 
-M = 1; N = 5. -> "1, 2, 3, 4, 5"
-M = 4; N = 8. -> "4, 6, 7, 8"
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
 */
+
+
+int GetSum(int num1, int num2)
+{
+    if (num1 == num2) return num2;
+    return (num1 + GetSum(num1 + 1, num2));
+}
+
+int m = 1; // Convert.ToInt32(Console.ReadLine());
+int n = 15; // Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {m}; N = {n} -> {GetSum(m, n)}");
