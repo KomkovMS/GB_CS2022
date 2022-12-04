de2dc25d5f28f59d7004ad5dfee82bc896e75f6f
KomkovMS <maxkoma05@gmail.com>
2022-12-04 15:54:15 +0700

    задача - посчитать количество положительных чисел в произвольно заданном массиве

---
+++ TaslListOther/task11/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача - посчитать количество положительных чисел в произвольно заданном массиве
+*/
+
+int getSumOfPositiveNumbers(int[] arr)
+{
+    int result = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) result += arr[i];
+    }
+
+    return result;
+}
+
+void PrintResult(int sum)
+{
+    Console.WriteLine($"Кол-во положительных чисел: {sum}");
+}
+
+
+int[] array = {-2, 0, -1, 3, 2};
+
+int sum = getSumOfPositiveNumbers(array);
+
+PrintResult(sum);
