e740130d7c8339d2e8270cb227189c6f62e11bbb
KomkovMS <maxkoma05@gmail.com>
2022-12-05 23:40:17 +0700

    задача - Записать алгоритм, который перенесет самое большое значение элемента в правый край массива (переработка по блок-схеме при попощи Пузырькового метода) - Ver.2

---
+++ TaslListOther/task16_2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 1. Нарисовать блок-схему
+Нахождение индексов максимального и минимального элемента
+массива
+*/
+
+
+// Вариант 2
+
+int[] arr = new int[]{41, -52, 1, 3, 0, -19, 333, 2, 1};
+int len = arr.Length;
+int idxMax = 0;
+int idxMin = 0;
+int max = arr[idxMax];
+int min = arr[idxMin];
+
+for (int i = 1; i < len; i++)
+{
+    if (arr[i] < min)
+    {
+        min = arr[i];
+        idxMin = i;
+    }
+    
+    else if (arr[i] > max)
+    {
+        max = arr[i];
+        idxMax = i;
+    }
+}
+
+Console.WriteLine($"Max = {max}, IdxMax = {idxMax}");
+Console.WriteLine($"Min = {min}, IdxMin = {idxMin}");
