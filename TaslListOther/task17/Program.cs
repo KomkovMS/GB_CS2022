9f8f32e150138caf02ca7577ec73c4e11111e579
KomkovMS <maxkoma05@gmail.com>
2022-12-07 00:11:04 +0700

    задача - Задание на разворот массива. Нужно перевернуть массив и записать его в обратном порядке

--- TaslListOther/task17/Program.cs
+++ TaslListOther/task17/Program.cs
@@ -5,13 +5,36 @@
 */
 
 
-int[] arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7};
-int len = arr.Length;
-int[] arr2 = arr;
+// int[] m = new int[] { 0, 1, 2, 3, 4, 5, 6, 7};
+// int n = m.Length;
+// int per = 0;
 
-for (int i = len - 1; i >= 0; i--)
+// for (int i = 0; i < n - 1 - i; i++)
+// {
+//     per += m[i];
+//     m[i] = m[n - 1 - i];
+//     m[n - 1 - i] = per;
+//     Console.WriteLine(m[i]);
+// }
+
+
+// вариант 2
+Console.WriteLine();
+
+int[] arr = new int[] {0, 1, 2, 3, 4, 5, 6, 7};
+Console.WriteLine(String.Join(',', arr));
+
+int len = arr.Length;
+int[] rev = new int[len];
+
+for (int i = 0; i < len; i++)
 {
-    arr2[i] = arr[i];
-    arr[i] = arr2[i];
-    Console.Write(arr[i]);
+    rev[len - 1 - i] = arr[i];
 }
+
+for (int i = 0; i < len; i++)
+{
+    arr[i] = rev[i];
+}
+
+Console.WriteLine(String.Join(',', arr));
