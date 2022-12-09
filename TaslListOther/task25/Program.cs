48eaccc53712f3e649410ab4cafc2298f19a04e9
KomkovMS <maxkoma05@gmail.com>
2022-12-09 22:14:22 +0700

    перенести самое большое значение элемента массива в правый край массива

---
+++ TaslListOther/task25/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача - перенести самое большое значение элемента массива в правый край массива
+[2, 5, 13, 7, 6, 4]
+*/
+
+int[] numbers = new int[] {2, 5, 13, 7, 6, 4};
+int index = 0;
+int size = numbers.Length;
+int maxIdx = 0;
+int max = numbers[maxIdx];
+int maximum = numbers[index];
+int maximumIndex = index;
+
+while (index < size)
+{
+    if (numbers[index] > maximum)
+    {
+        maximum = numbers[index];
+        maximumIndex = index;
+    }
+    index += 1;
+}
+
+numbers[maximumIndex] = numbers[size - 1];
+numbers[size - 1] = maximum;
+
+Console.WriteLine($"[{string.Join(", ", numbers)}]"); // {2, 5, 7, 6, 4, 13}
